34b6444f0fce390824ec41b99e131918223993f1
Christopher Deutsch <cd@cdeutsch.com>
2022-04-11 15:06:16 -0500

    Add missing types folder 😬

---
+++ src/types/APLQuery.ts
@@ -0,0 +1,5 @@
+import { APLRequestWithOptions } from './codegenApiTypes';
+
+// Make a more friendly type name for axiom-elements consumers.
+// The codegen names aren't great.
+export type APLQuery = APLRequestWithOptions;
